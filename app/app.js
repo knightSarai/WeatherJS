a614f3f3e24486478315bef42979f05b915d86f5
knightSarai <knight@DESKTOP-EP69NRM.localdomain>
2020-11-15 15:49:30 +0200

    finish the main functionalty

--- app/app.js
+++ app/app.js
@@ -11,20 +11,38 @@ WeatherApp.config(function ($routeProvider) {
             templateUrl: 'pages/forcast.html',
             controller: 'forcastController'
         })
+        .when('/forcast/:days', {
+            templateUrl: 'pages/forcast.html',
+            controller: 'forcastController'
+        })
 });
 // SERVICES
 WeatherApp.service('cityService', function () {
-    this.city = "New York, NY";
-})
-//CONTROLLER
-WeatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
-    $scope.city = cityService.city;
-    $scope.$watch('city', () => {
-        cityService.city = $scope.city;
-    })
-}]);
+    this.city = "Amman";
+});
 
-WeatherApp.controller('forcastController', ['$scope', 'cityService', function ($scope, cityService) {
-    $scope.city = cityService.city;
-}]);
+//CONTROLLER
+WeatherApp.controller('homeController', ['$scope', 'cityService',
+    function ($scope, cityService) {
+        $scope.city = cityService.city;
+
+        $scope.$watch('city', function () {
+            cityService.city = $scope.city;
+        })
+    }]);
+
+WeatherApp.controller('forcastController', ['$scope', '$http', '$routeParams', 'cityService',
+    function ($scope, $http, $routeParams, cityService) {
+        $scope.city = cityService.city;
+        /** API request */
+        const appid = 'bd46242ed283bdb91ab51b08972175c3';
+        const days = $routeParams.days | 2;
+        $http
+            .get(`http://api.openweathermap.org/data/2.5/forecast?q=${$scope.city}&cnt=${days}&appid=${appid}`)
+            .then(res => $scope.weatherResult = res.data)
+            .then(() => console.log($scope.weatherResult));
+        $scope.convertToCelsius = degK => Math.round(degK - 273.15)
+        /**Watcher */
+    }
+]);
 
